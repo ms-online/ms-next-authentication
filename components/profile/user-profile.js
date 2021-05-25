5190185f8c79f1e246bd9ea652b841b60edde3bb
ms-online <27732357@qq.com>
2021-05-25 20:59:58 +0800

    lesson-9

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -4,23 +4,23 @@ import ProfileForm from './profile-form'
 import classes from './user-profile.module.css'
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true)
+  // const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(
-    () =>
-      getSession().then((session) => {
-        if (!session) {
-          window.location.href = '/auth'
-        } else {
-          setIsLoading(false)
-        }
-      }),
-    []
-  )
+  // useEffect(
+  //   () =>
+  //     getSession().then((session) => {
+  //       if (!session) {
+  //         window.location.href = '/auth'
+  //       } else {
+  //         setIsLoading(false)
+  //       }
+  //     }),
+  //   []
+  // )
 
-  if (isLoading) {
-    return <p>loading...</p>
-  }
+  // if (isLoading) {
+  //   return <p>loading...</p>
+  // }
   return (
     <section className={classes.profile}>
       <h1>用户详情页面</h1>
